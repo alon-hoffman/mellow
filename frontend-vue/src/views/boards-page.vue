525d0a7af6346b8d71cca09d8c73ec75232af955
Cedrus81 <erez.eitan.97@gmail.com>
2022-11-30 14:08:43 +0200

    gallery UI

--- frontend-vue/src/views/boards-page.vue
+++ frontend-vue/src/views/boards-page.vue
@@ -1,9 +1,60 @@
 <template>
     <section class="container boards-page">
-      <h3 class="recently-viewed">
+      <h3 class="gallery-header">
           <img src="../assets/icons/time.png" alt="">
-        Recently viewed</h3>
-      
+        Recently viewed
+      </h3>
+      <ul class="gallery flex wrap">
+        <li class="gallery-item">
+          <div class="board-preview">
+            Traco
+            <div class="board-preview-options">
+              <img class="icon" src="../assets/icons/star_regular.svg" alt="star-regular">
+            </div>
+          </div>
+        </li>
+        <li class="gallery-item">
+          <div class="board-preview">
+            Dummy board
+            <div class="board-preview-options">
+              <img class="icon" src="../assets/icons/star_regular.svg" alt="star-regular">
+            </div>
+          </div>
+        </li>
+        <li class="gallery-item">
+          <div class="board-preview">
+            Dummy board
+            <div class="board-preview-options">
+              <img class="icon" src="../assets/icons/star_regular.svg" alt="star-regular">
+            </div>
+          </div>
+        </li>
+        <li class="gallery-item">
+          <div class="board-preview">
+            Dummy board
+            <div class="board-preview-options">
+              <img class="icon" src="../assets/icons/star_regular.svg" alt="star-regular">
+            </div>
+          </div>
+        </li>
+        <li class="gallery-item">
+          <div class="board-preview">
+            Dummy board
+            <div class="board-preview-options">
+              <img class="icon" src="../assets/icons/star_regular.svg" alt="star-regular">
+            </div>
+          </div>
+        </li>
+        <!-- <li class="board-preview">Dummy board</li>
+        <li class="board-preview">Dummy board</li>
+        <li class="board-preview">Dummy board</li>
+        <li class="board-preview">Dummy board</li> -->
+
+        <!-- <li class="board-preview" v-for="board in boards">
+          Dummy board
+        </li> -->
+      </ul>
+
     </section>
   
     
