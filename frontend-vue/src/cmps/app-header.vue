35e435b8c79c567ab3b1748dda125bc0af0dbae1
itai <itaimorag2000@gmail.com>
2022-11-30 09:31:27 +0200

    sdasd

--- frontend-vue/src/cmps/app-header.vue
+++ frontend-vue/src/cmps/app-header.vue
@@ -14,6 +14,36 @@
       <router-link to="/login">Login / Signup</router-link>
     </nav>
   </header>
+  <header v-if="hello==='/boardsPage'" class="boards-page">
+    <nav>
+      <router-link to="/">
+        <img class="logo-img-home" src="https://d2k1ftgv7pobq7.cloudfront.net/meta/c/p/res/images/trello-header-logos/167dc7b9900a5b241b15ba21f8037cf8/trello-logo-blue.svg" alt="">
+      </router-link>
+
+     
+      <router-link to="/boards">Board app</router-link>
+
+
+      <router-link to="/boardsPage">Mellow</router-link>
+      <router-link to="/">home</router-link>
+      <router-link to="/login">Login / Signup</router-link>
+    </nav>
+  </header>
+  <header v-if="hello==='/boards'" class="board-gallery">
+    <nav>
+      <router-link to="/">
+        <img class="logo-img-home" src="https://d2k1ftgv7pobq7.cloudfront.net/meta/c/p/res/images/trello-header-logos/167dc7b9900a5b241b15ba21f8037cf8/trello-logo-blue.svg" alt="">
+      </router-link>
+
+     
+      <router-link to="/boards">Board app</router-link>
+
+
+      <router-link to="/boardsPage">Mellow</router-link>
+      <router-link to="/">home</router-link>
+      <router-link to="/login">Login / Signup</router-link>
+    </nav>
+  </header>
 </template>
 <script>
 export default {
