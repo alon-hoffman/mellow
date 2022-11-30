50f7a9ee520fa45d2c534f12b249999e4cbefde4
Alon Hoffman <a.hoffman.lon@gmail.com>
2022-11-30 11:22:35 +0200

    Added html to board-detail

--- frontend-vue/src/cmps/group-list.vue
+++ frontend-vue/src/cmps/group-list.vue
@@ -1,29 +1,25 @@
 <template>
-    <section class="group-list">
-<h1>group-list</h1>
-    </section>
-    
-  </template>
-  
-  <script>
+  <section class="card-group-list">
+    <h1>group-list</h1>
+    <ul class="clean-list">
+    <card-group v-for="list in lists"/>
+  </ul>
+  </section>
+</template>
 
-  export default {
-    data() {
-      return {
-       
-      }
-    },
-    computed: {
-      
-    },
-    created() {
-      
-    },
-    methods: {
-      
-    }
-  
-    
-  }
-  </script>
-  
+<script>
+import cardGroup from "../cmps/card-group.vue";
+export default {
+  data() {
+    return {
+      // lists=["","","","","","",]
+    };
+  },
+  components: {
+    cardGroup,
+  },
+  computed: {},
+  created() {},
+  methods: {},
+};
+</script>
