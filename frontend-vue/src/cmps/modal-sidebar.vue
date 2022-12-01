b24b4207363430af0a00cf89cb25865f64bfd9fe
itai <itaimorag2000@gmail.com>
2022-12-01 11:35:33 +0200

    add img filter

--- frontend-vue/src/cmps/modal-sidebar.vue
+++ frontend-vue/src/cmps/modal-sidebar.vue
@@ -1,12 +1,22 @@
 <template>
-<section class="modal-add-menu">
-
-</section>
+    <section class="modal-add-menu">
+        <h4>Add to card</h4>
+         <div class="fake-button"><img src="../assets/icons/person.svg" alt="">Members</div>
+         <div class="fake-button"></div>
+         <div class="fake-button"></div>
+         <div class="fake-button"></div>
+         <div class="fake-button"></div>
+         <div class="fake-button"></div>
+    </section>
 </template>
 
 
 <script>
 export default {
-
+    props: {
+        card: {
+            type: Object
+        }
+    }
 }
 </script>
