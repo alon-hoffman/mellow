fa16bcb0b2f7d6db90084d7de2b6ce99ca7137c4
Cedrus81 <erez.eitan.97@gmail.com>
2022-12-02 00:12:26 +0200

    work on crud-flow

--- frontend-vue/src/store/board.store.js
+++ frontend-vue/src/store/board.store.js
@@ -26,9 +26,9 @@ export const boardStore = {
     },
     getters: {
         boards({ boards }) { return boards },
-        getCurrBoard(state) {return state.currBoard},
-        getMembersOfBoard(state){return state.currBoard.members},
-        getLabelsOfBoard(state){return state.currBoard.labels},
+        getCurrBoard(state) { return state.currBoard },
+        getMembersOfBoard(state) { return state.currBoard.members },
+        getLabelsOfBoard(state) { return state.currBoard.labels },
     },
     mutations: {
         setBoards(state, { boards }) {
@@ -47,7 +47,7 @@ export const boardStore = {
         setBoardById(state, { _id }) {
             const board = JSON.parse(JSON.stringify(state.boards))[0];
             // const board = state.boards.find(c => c.id === _id)
-            state.currBoard=board
+            state.currBoard = board
         },
 
     },
@@ -90,6 +90,15 @@ export const boardStore = {
                 throw err
             }
         },
+        async saveList({ commit, dispatch, state }, { list }) {
+            console.log('saveList', list)
+            try {
+
+            } catch (err) {
+                console.log('Error, could not Add or update list')
+                throw err
+            }
+        }
 
     }
 }
