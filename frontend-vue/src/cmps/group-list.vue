25cd9a4cab4abdd9ce4b5f45380b045367b80acb
Alon Hoffman <a.hoffman.lon@gmail.com>
2022-12-01 15:31:00 +0200

    Create list added

--- frontend-vue/src/cmps/group-list.vue
+++ frontend-vue/src/cmps/group-list.vue
@@ -1,20 +1,31 @@
 <template>
   <section v-if="lists" class="group-list">
     <ul class="clean-list">
-    <card-group v-for="list in lists" :list="list" :key="list.id" @cardEdit="$emit('cardEdit', $event)"/>
-  </ul>
+      <card-group @edit="$emit('edit')"
+        v-for="list in lists"
+        :list="list"
+        :key="list.id"
+        @cardEdit="$emit('cardEdit', $event)"
+      />
+      <button v-if="!isNewListEdit" @click="(isNewListEdit=true)">Add another list</button>
+      <form v-else @submit.prevent="addList"  v-click-outside="closeEdit" >
+        <input type="text" v-model="newTitle"/>
+        <input type="submit">
+      </form>
+    </ul>
   </section>
 </template>
 
 <script>
 import cardGroup from "../cmps/card-group.vue";
 export default {
-  props:{
-    lists:Array,
+  props: {
+    lists: Array,
   },
   data() {
     return {
-      
+      isNewListEdit:false,
+      newTitle:""
     };
   },
   components: {
@@ -22,6 +33,16 @@ export default {
   },
   computed: {},
   created() {},
-  methods: {},
+  methods: {
+    addList(){
+      this.isNewListEdit=false
+      this.$emit('addList', this.newTitle)
+    },
+    closeEdit(){
+      this.isNewListEdit=false
+    }
+  },
 };
 </script>
+
+<!-- $emit('addList') -->
